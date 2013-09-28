afc0ffd776250e9a753e73710d3313a0f6eb29a3
Andre Farzat <andrefarzat@gmail.com>
2013-09-28 01:16:38 -0400

    Updating the initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-styleguide',
+    version='0.1.0',
+    author=u'André Farzat',
+    author_email='andrefarzat@gmail.com',
+    packages=find_packages(),
+    url='http://pypi.python.org/pypi/django-styleguide/',
+    license='LICENSE',
+    description='Styleguide for django projects',
+    long_description=open('README.md').read(),
+    install_requires=["Django >= 1.4.3",],
+    include_package_data=True,
+    zip_safe=False,
+)
