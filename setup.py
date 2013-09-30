887c7da2749783c92e567efdbc75dc64e5883782
Andre Farzat <andrefarzat@gmail.com>
2013-09-30 01:45:39 -0400

    Changing version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-styleguide',
-    version='0.1.1',
+    version='0.1.2',
     author=u'André Farzat',
     author_email='andrefarzat@gmail.com',
     packages=find_packages(),
