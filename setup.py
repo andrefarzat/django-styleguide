5fcec12b1018960f1f139770c1e48f09f85ccd9c
Andre Farzat <andrefarzat@gmail.com>
2014-01-16 14:05:05 -0500

    adding  python 2.4 2.5 and 2.6 to travis.yml to test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-styleguide',
-    version='0.1.13',
+    version='0.1.14',
     author=u'André Farzat',
     author_email='andrefarzat@gmail.com',
     packages=find_packages(),
