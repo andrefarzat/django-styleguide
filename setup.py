8122789fd42406e085c6215677b837f7f9a3f13b
Andre Farzat <andrefarzat@gmail.com>
2014-09-15 22:44:38 -0400

    Version 0.1.18

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages
 
 setup(
     name='django-styleguide',
-    version='0.1.17',
-    author=u'André Farzat',
+    version='0.1.18',
+    author=u'Andre Farzat',
     author_email='andrefarzat@gmail.com',
     packages=find_packages(),
     url='http://pypi.python.org/pypi/django-styleguide/',
