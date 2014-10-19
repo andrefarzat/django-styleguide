43729cdffe9711d1f23d4286985a964c389f761b
Andre Farzat <andrefarzat@gmail.com>
2014-10-19 19:06:02 -0400

    adding middleware requirements to django 1.7

--- test_settings.py
+++ test_settings.py
@@ -20,6 +20,12 @@ DATABASES = {
     }
 }
 
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
